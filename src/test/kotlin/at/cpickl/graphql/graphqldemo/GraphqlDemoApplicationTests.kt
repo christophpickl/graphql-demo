c00d18001eea77daa7777b9559b56072fae147c2
christophpickl <christoph.pickl@gmail.com>
2017-11-14 05:57:48 +0100

    import spring starter template

---
+++ src/test/kotlin/at/cpickl/graphql/graphqldemo/GraphqlDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package at.cpickl.graphql.graphqldemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class GraphqlDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
